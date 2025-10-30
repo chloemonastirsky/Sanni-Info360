99b109742449de153acd6c9ff125a3b0ee7c88f6
CHLOE MONASTIRSKY <49552251@ort>
2025-10-30 14:28:57 -0300

    ultimo commit

--- obj/Debug/net8.0/Sanni.AssemblyInfo.cs
+++ obj/Debug/net8.0/Sanni.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sanni")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22bab51b13d715699294c57b90ff5116df394aed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be91737ca0412d9a821f4420e7c34120f9577506")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sanni")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sanni")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
