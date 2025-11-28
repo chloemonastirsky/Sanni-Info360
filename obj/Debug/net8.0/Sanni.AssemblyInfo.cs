2110fca409f1faca3528ae645e901165675389fb
CHLOE MONASTIRSKY <49552251@ort>
2025-11-28 14:20:10 -0300

    oa

--- obj/Debug/net8.0/Sanni.AssemblyInfo.cs
+++ obj/Debug/net8.0/Sanni.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sanni")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b2af30fa4b786d54f4bda0c5cc6d448924cecbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b38a58b95e3938e62bdeddb94a582d47d67192ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sanni")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sanni")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
