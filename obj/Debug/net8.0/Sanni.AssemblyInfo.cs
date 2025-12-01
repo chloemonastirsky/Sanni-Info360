92772edd283ba3bff79f9364d6d4e4bf48f56d51
CHLOE MONASTIRSKY <49552251@ort>
2025-12-01 13:18:26 -0300

    jskljadl

--- obj/Debug/net8.0/Sanni.AssemblyInfo.cs
+++ obj/Debug/net8.0/Sanni.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sanni")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2110fca409f1faca3528ae645e901165675389fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65d1331588998aa0adf45dabde2b527be6a5a7b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sanni")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sanni")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
